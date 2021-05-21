e16ab3c8c300de56bf787d4d33e1034790e5cb02
M.Sriram <43115330+msris108@users.noreply.github.com>
2021-05-21 10:47:40 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle('dark-mode');
+}
